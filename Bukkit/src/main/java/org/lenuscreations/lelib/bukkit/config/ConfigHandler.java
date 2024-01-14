f90410870ed391dfef76b622ac0b601eda6208d2
grcq <me@grcq.dev>
2024-01-14 22:24:36 +0100

    Something

---
+++ Bukkit/src/main/java/org/lenuscreations/lelib/bukkit/config/ConfigHandler.java
@@ -0,0 +1,12 @@
+package org.lenuscreations.lelib.bukkit.config;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConfigHandler {
+
+
+}
---
+++ Bukkit/src/main/java/org/lenuscreations/lelib/bukkit/config/Configuration.java
@@ -0,0 +1,19 @@
+package org.lenuscreations.lelib.bukkit.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Configuration {
+
+    String value() default "config";
+    Type type() default Type.YAML;
+
+    enum Type {
+        YAML
+    }
+
+}
